8ec119892bffc8cbcbe3edacf5663ed221391bfd
Usuario <Usuario@DESKTOP-VJT583G>
2019-12-10 13:42:10 -0300

    C# é bem de boas de programar

---
+++ Teste/MatriculaNatacao/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace MatriculaNatacao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCategoria = new System.Windows.Forms.TextBox();
+            this.btnGeraCategoria = new System.Windows.Forms.Button();
+            this.lblHojeEh = new System.Windows.Forms.Label();
+            this.dtpNascimento = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(56, 6);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(299, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ano de nascimento:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Categoria:";
+            // 
+            // txtCategoria
+            // 
+            this.txtCategoria.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtCategoria.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCategoria.ForeColor = System.Drawing.Color.Black;
+            this.txtCategoria.Location = new System.Drawing.Point(76, 74);
+            this.txtCategoria.Name = "txtCategoria";
+            this.txtCategoria.ReadOnly = true;
+            this.txtCategoria.Size = new System.Drawing.Size(105, 20);
+            this.txtCategoria.TabIndex = 7;
+            this.txtCategoria.TextChanged += new System.EventHandler(this.txtCategoria_TextChanged);
+            // 
+            // btnGeraCategoria
+            // 
+            this.btnGeraCategoria.Location = new System.Drawing.Point(370, 70);
+            this.btnGeraCategoria.Name = "btnGeraCategoria";
+            this.btnGeraCategoria.Size = new System.Drawing.Size(139, 27);
+            this.btnGeraCategoria.TabIndex = 8;
+            this.btnGeraCategoria.Text = "Gerar categoria";
+            this.btnGeraCategoria.UseVisualStyleBackColor = true;
+            this.btnGeraCategoria.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblHojeEh
+            // 
+            this.lblHojeEh.AutoSize = true;
+            this.lblHojeEh.Location = new System.Drawing.Point(367, 41);
+            this.lblHojeEh.Name = "lblHojeEh";
+            this.lblHojeEh.Size = new System.Drawing.Size(41, 13);
+            this.lblHojeEh.TabIndex = 9;
+            this.lblHojeEh.Text = "Hoje é ";
+            // 
+            // dtpNascimento
+            // 
+            this.dtpNascimento.Location = new System.Drawing.Point(122, 41);
+            this.dtpNascimento.Name = "dtpNascimento";
+            this.dtpNascimento.Size = new System.Drawing.Size(167, 20);
+            this.dtpNascimento.TabIndex = 10;
+            this.dtpNascimento.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 115);
+            this.Controls.Add(this.dtpNascimento);
+            this.Controls.Add(this.lblHojeEh);
+            this.Controls.Add(this.btnGeraCategoria);
+            this.Controls.Add(this.txtCategoria);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCategoria;
+        private System.Windows.Forms.Button btnGeraCategoria;
+        private System.Windows.Forms.Label lblHojeEh;
+        private System.Windows.Forms.DateTimePicker dtpNascimento;
+    }
+}
+
---
+++ Teste/MatriculaNatacao/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatriculaNatacao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lblHojeEh.Text = "Hoje é " + DateTime.Now.ToShortDateString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TimeSpan TsQuantidadeDeDias = DateTime.Now.Date - dtpNascimento.Value;
+            int diferenca = TsQuantidadeDeDias.Days / 365;
+            string categoria = null;
+            if(txtNome.Text == string.Empty)
+            {
+                MessageBox.Show("POR FAVOR, PREENCHA TODOS OS CAMPOS", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (diferenca >= 5 && diferenca <= 7)
+                {
+                    categoria = "INFANTIL A";
+                }
+                else if (diferenca >= 8 && diferenca <= 10)
+                {
+                    categoria = "INFANTIL B";
+                }
+                else if (diferenca >= 11 && diferenca <= 13)
+                {
+                    categoria = "JUVENIL A";
+                }
+                else if (diferenca >= 14 && diferenca <= 17)
+                {
+                    categoria = "JUVENIL B";
+                }
+                else if (diferenca >= 18)
+                {
+                    categoria = "ADULTO";
+                }
+                else if (diferenca < 5 && diferenca > 0)
+                {
+                    MessageBox.Show("NAO EXISTE CATEGORIA", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show("A IDADE DE NASCIMENTO DEVE SER MENOR QUE A IDADE DE ULTIMO ANIVERSÁRIO", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                txtCategoria.Text = categoria;
+            }
+        }
+
+        private void txtCategoria_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
